c8d3b1ae2c04f860e3c29724b0bdba0ef2ec995a
pall-dev <126366916+pall-dev@users.noreply.github.com>
2023-12-08 00:40:13 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,19 @@
-const wrapper = document.querySelector(".wrapper");
-const question = document.querySelector(".question");
-const gif = document.querySelector(".gif");
-const yesBtn = document.querySelector(".yes-btn");
-const noBtn = document.querySelector(".no-btn");
-
-yesBtn.addEventListener("click", () => {
-    question.innerHTML = "Aaaaaa, I Like You Too";
-    gif.src = "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
-    hideNoButton();
-});
-
-noBtn.addEventListener("click", () => {
-    moveNoButton();
-});
-
-noBtn.addEventListener("mouseover", () => {
-    moveNoButton();
-});
-
-function moveNoButton() {
-    const maxX = window.innerWidth - noBtn.clientWidth;
-    const maxY = window.innerHeight - noBtn.clientHeight;
-
-    const randomX = Math.floor(Math.random() * maxX);
-    const randomY = Math.floor(Math.random() * maxY);
-
-    noBtn.style.left = randomX + "px";
-    noBtn.style.top = randomY + "px";
+function login() {
+    // Implement login logic here
+    alert("Login function called.");
 }
 
-function hideNoButton() {
-    noBtn.style.display = "none";
+function register() {
+    // Implement register logic here
+    alert("Register function called.");
+}
+
+function showRegisterForm() {
+    document.getElementById("loginForm").style.display = "none";
+    document.getElementById("registerForm").style.display = "block";
+}
+
+function showLoginForm() {
+    document.getElementById("registerForm").style.display = "none";
+    document.getElementById("loginForm").style.display = "block";
 }
